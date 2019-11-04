fed16d710dadcb93747a4f5d6e352d9dc47e2b3d
manish-eng <52404993+manish-eng@users.noreply.github.com>
2019-11-04 13:09:12 +0530

    updated setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 from setuptools import setup, find_packages
 
-setup(name=‘pms’,
+setup(name='pms',
       version='1.0',
-      packages=find_packages(),
-      scripts=['manage.py'])
+      packages=find_packages())
